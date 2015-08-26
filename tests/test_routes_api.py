5a012d6f37b72b2420cd30d7ef4d3e2b844bff3c
Andrei Sura <sura.andrei@gmail.com>
2015-08-26 16:45:17 -0400

    Improve tests/test_routes_api.py

--- tests/test_routes_api.py
+++ tests/test_routes_api.py
@@ -8,6 +8,7 @@ from app.routes.api import check_email_config
 class RoutesApiTests(BaseTestCase):
 
     def test_check_email_config(self):
+        """ Fail when nothing set """
         errors = [
             'Emailing param MAIL_USERNAME was not configured.',
             'Emailing param MAIL_PASSWORD was not configured.',
@@ -16,3 +17,13 @@ class RoutesApiTests(BaseTestCase):
         passed, actual_errors = check_email_config()
         self.assertFalse(passed)
         self.assertEquals(errors, actual_errors)
+
+    def test_check_email_config_ok(self):
+        """ Pass when all set """
+        self.app.config['MAIL_USERNAME'] = 'xyz'
+        self.app.config['MAIL_PASSWORD'] = 'xyz'
+        self.app.config['MAIL_SERVER'] = 'xyz'
+        self.app.config['MAIL_PORT'] = 'xyz'
+        passed, actual_errors = check_email_config()
+        self.assertTrue(passed)
+        self.assertEquals([], actual_errors)
